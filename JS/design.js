566a1528ca8603becb70cc729f0cabde456ac69b
sumyta12 <ois42168@gmail.com>
2023-03-27 19:23:57 +0600

    first commit

---
+++ JS/design.js
@@ -0,0 +1,5 @@
+const round_youtube_video = document.querySelector(".round");
+round_youtube_video.addEventListener("click", function () {
+  const video_page = document.querySelector(".video_page");
+  video_page.classList.toggle("video_page_display");
+});
